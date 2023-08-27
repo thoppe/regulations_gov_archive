c70c9c16516ea6d9abca3f0c1632c122f0247695
thoppe <travis.hoppe@gmail.com>
2023-08-27 17:32:47 -0400

    Add more daily results

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ all:
 lint:
 	black *.py --line-length 80
 	flake8 *.py --ignore=E501
+
+add_data:
+	git add data/daily_search_results
--- P2_collect_documents.py
+++ P2_collect_documents.py
@@ -13,8 +13,11 @@ df = pd.read_csv(
 )
 df[date_key] = pd.to_datetime(df[date_key]).dt.date
 
+# Filter out withdrawn documents
+df = df[df["attributes.withdrawn"] == False]
+
 # Shuffle to get a good sampling
-df = df.sample(frac=1, random_state=23)
+df = df.sample(frac=1, random_state=102)
 
 buffer_days = 30
 check_time = datetime.now().date() - timedelta(days=buffer_days)
@@ -38,26 +41,49 @@ def compute(docID, f1):
     js = r.json()
 
     if not r.ok:
-        raise ValueError(r.status_code, r.content)
+        if r.status_code == 404:
+            print(f"404 {docID} skipping")
+            return
 
-    title = js["data"]["attributes"]["title"]
+        raise ValueError(docID, r.status_code, r.content)
+
+    attrs = js["data"]["attributes"]
+    title = attrs["title"]
+
+    # Debugging statement
+    # print(json.dumps(attrs, indent=2))
+
+    # Error checking
+    if "fileFormats" not in attrs:
+        print(attrs)
+        raise KeyError("Missing fileFormat")
+
+    # IF the key exists but is null use an empty list
+    if attrs["fileFormats"] is None:
+        attrs["fileFormats"] = []
 
     # Try to download the HTM, we need to find out which one
     target_download = None
-    for row in js["data"]["attributes"]["fileFormats"]:
+
+    for row in attrs["fileFormats"]:
         if row["format"] == "htm":
             target_download = row
 
     # Raise an Error if not found
-    assert target_download
+    # assert target_download
 
-    r2 = session.get(target_download["fileUrl"])
-    if not r2.ok:
-        raise ValueError(r2.status_code, r2.content)
+    if target_download is not None:
+        r2 = session.get(target_download["fileUrl"])
+        if not r2.ok:
+            raise ValueError(r2.status_code, r2.content)
 
-    soup = bs4.BeautifulSoup(r2.text, "html.parser")
-    text = soup.text.strip()
-    js["data"]["content"] = {"text": text}
+        soup = bs4.BeautifulSoup(r2.text, "html.parser")
+        text = soup.text.strip()
+        js["data"]["content"] = {"text": text}
+
+    else:
+        print("Missing htm fileFormat", attrs["fileFormats"])
+        text = ""
 
     with open(f1, "w") as FOUT:
         json.dump(js, FOUT)
